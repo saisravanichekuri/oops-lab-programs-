eede30eded3513bfa2148f086283cdec1d551f82
saisravanichekuri <158797811+saisravanichekuri@users.noreply.github.com>
2024-02-29 11:14:01 +0530

    Create PriInheri.cpp

---
+++ week5/PriInheri.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class Parent{
+    public:
+    int publicVar=10;
+
+    private:
+    int privateVar=20;
+
+    protected:
+    int protectedVar=30;
+
+    public:
+    int getPrivateVar(){
+       return privateVar;
+    }
+};
+
+class Child : private Parent{
+    public:
+    int proTemp = protectedVar;
+    int pubTemp = publicVar;
+    int priTemp = getPrivateVar();
+};
+
+int main(){
+    Child obj;
+    cout<<"Public variable is "<<obj.pubTemp<<endl;
+
+    cout<<"Private Variable is "<<obj.priTemp<<endl;
+    //obj.getPrivateVar();
+    
+    //cout<<"Portected Variable is "<<obj.protectedVar<<endl;
+    cout<<"Portected Variable is "<<obj.proTemp<<endl;
+}
