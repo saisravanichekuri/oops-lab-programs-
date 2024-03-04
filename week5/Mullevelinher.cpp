7eef4c28da86109e874130e26b43bb62ee8c21e0
saisravanichekuri <158797811+saisravanichekuri@users.noreply.github.com>
2024-03-04 21:32:27 +0530

    Update Mullevelinher.cpp

--- week5/Mullevelinher.cpp
+++ week5/Mullevelinher.cpp
@@ -1,22 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Vehicle{
-    public:
-    void vehicle(){
-        cout<<"i am a vehicle"<<endl;
+
+class Vehicle {
+public:
+    void vehicle () {
+        cout << "Vehicle engine started" << endl;
     }
 };
-class Fourwheeler:public Vehicle{
-        public: Fourwheeler(){
-        cout<<"i am a four wheeler vehicle"<<endl;
+
+class Fourwheeler : public Vehicle {
+public:
+    void four() {
+        cout << "i am a four wheeler vehicle" << endl;
     }
 };
-class car:public Fourwheeler{
-    public: car(){
-        cout<<"car is a four wheeler vehicle"<<endl;
+
+class Car : public Fourwheeler {
+public:
+    void car() {
+        cout << "car is a fourwheeler vehicle" << endl;
     }
 };
-int main(){
-    car obj;
+
+int main() {
+    Car obj;
+    obj.vehicle();
+    obj.four();
+    obj.car();
     return 0;
 }
