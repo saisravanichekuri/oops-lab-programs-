ae0511588a4478f33d0271ef18136d3178d20c97
saisravanichekuri <158797811+saisravanichekuri@users.noreply.github.com>
2024-03-04 21:40:44 +0530

    Update Multipleinher.cpp

--- week5/Multipleinher.cpp
+++ week5/Multipleinher.cpp
@@ -7,12 +7,14 @@ class Vehicle{
     }
 };
 class Fourwheeler{
-    public: Fourwheeler(){
+    public: four(){
         cout<<"i am a four wheeler vehicle"<<endl;
     }
 };
 class car: public Vehicle, public Fourwheeler{};
 int main(){
     car obj;
+    car.vehicle();
+    car.four();
     return 0;
 }
