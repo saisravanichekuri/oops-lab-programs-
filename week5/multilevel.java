57f063c0d9f79b3881d1197baa5a9fb032950c04
saisravanichekuri <158797811+saisravanichekuri@users.noreply.github.com>
2024-03-05 21:48:50 +0530

    Update multilevel.java

--- week5/multilevel.java
+++ week5/multilevel.java
@@ -7,7 +7,7 @@ class Dog extends Animal{
 class BabyDog extends Dog{
     void weep(){System.out.println("weeping");}
 }
-class TestInheritance{
+public class TestInheritance{
     public static void main(String args[]){
         BabyDog d=new BabyDog();
         d.weep();
