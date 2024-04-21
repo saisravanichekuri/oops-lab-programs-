bfabf21811da9501fda7c15d4f8f75a31e8b4fba
saisravanichekuri <158797811+saisravanichekuri@users.noreply.github.com>
2024-04-21 16:51:04 +0530

    Update copycommand.java

--- week9/copycommand.java
+++ week9/copycommand.java
@@ -1,19 +1,22 @@
 import java.io.*;
-class copycommand {
+
+class CopyCommand{
     public static void main(String[] args) {
         FileInputStream input;
-        FileOutputStream output;
+        FileOutputStream  output;
         try{
-            input=new FileInputStream("source");
-            output=new FileOutputStream(" destination");
+            input = new FileInputStream("source");
+            output = new FileOutputStream("Destination");
             while(input.available()>0){
                 output.write(input.read());
             }
-        }catch(FileNotFoundException obj){
-            System.out.println("Invalid file name or file not found exception");
-        }catch(IOException i){
-             System.out.println("IO ERROR");
         }
-
+        catch(FileNotFoundException obj){
+            System.out.println("File Not Found!");
+        }
+        catch(IOException obj){
+            System.out.println("Input/Output Exception!");
+        }
     }
 }
+
